91a5ffa097f02db0c7df2bd8897465a43b66df78
sirapobsi <sirapob_si@cmu.ac.th>
2016-08-21 02:17:12 +0700

    add python1

---
+++ test_prime.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+
+def main():
+    x = int(input(""))
+    factorize(x)
+
+def factorize(x):
+    m = 2
+    p = x
+    print("{0} is".format(x) ,end=" ")
+    while (m <= (p)**0.5):
+        if (x % m == 0):
+            if (x == p): 
+               print(m ,end="")
+            else:
+               print(" x {0}" .format(m) ,end="")
+            x = x//m
+        elif (x % m != 0):
+           m = m + 1
+    if (x == p):
+        print("prime",end="")
+    elif (x != 1):
+        print(" x {0}".format(x),end="")
+    print()
+
+if __name__ == '__main__':
+    main()
+        
+        
